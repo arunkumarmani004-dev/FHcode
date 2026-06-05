00c8227b8405be487dde02839b90cbc83528541c
arukumarmani004-dev <arunkumarmani004@gmail.com>
2026-06-05 11:43:17 +0530

    js edited

--- varar.js
+++ varar.js
@@ -161,31 +161,31 @@
 //     console.log(row);
 // }
 
-// for(let i = 1; i <= 5; i++) {
+for(let i = 1; i <= 5; i++) {
 
-//     let row = "";
+    let row = "";
 
-//     for(let j = 1; j <= i; j++) {
-//         row = row + "*";
-//     }
+    for(let j = 1; j <= i; j++) {
+        row = row + "*";
+    }
 
-//     console.log(row);
-// }
+    console.log(row);
+}
 
-// let n=7
-// for (let i = n - 1; i >= 1; i--) {
-//     let row = "";
+let n=7
+for (let i = n - 1; i >= 1; i--) {
+    let row = "";
 
-//     for (let j = 1; j <= n - i; j++) {
-//         row += " ";
-//     }
+    for (let j = 1; j <= n - i; j++) {
+        row += " ";
+    }
 
-//     for (let k = 1, k <= 2 * i - 1, k++) {
-//         row += "*";
-//     }
+    for (let k = 1, k <= 2 * i - 1, k++) {
+        row += "*";
+    }
 
-//     console.log(row);
-// }
+    console.log(row);
+}
 
 let person={
     name: "ramya",
